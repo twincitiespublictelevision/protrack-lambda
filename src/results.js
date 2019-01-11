2abec778fc388b015aa64f2ab0b1403602caf82f
Augustus Mayo <gusmayo@gmail.com>
2019-01-11 14:57:23 -0600

    TPTOR-2798
    Use episode id from results

--- src/results.js
+++ src/results.js
@@ -1,11 +1,7 @@
 import { normalize as normalizer, schema } from 'normalizr';
 
 let show = new schema.Entity('show');
-let episode = new schema.Entity('episode', {}, {
-  idAttribute: function(episode) {
-    return `${episode.program.id}${episode.version.id}`;
-  }
-});
+let episode = new schema.Entity('episode');
 let airing = new schema.Entity('airing', { show, episode }, {
   idAttribute: function(airing) {
     return `${airing.channel}.${airing.date}`;
